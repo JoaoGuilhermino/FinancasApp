7ec8d64febc2ea7ff63f36888943c5f73656b062
JoaoGuilhermino <jvgcosta.bt@gmail.com>
2025-10-07 20:32:32 -0300

    Criação do Smtp e RabbitMQ para mensageria.

---
+++ FinancasApp.Domain/Interfaces/Messages/IMessageProducer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinancasApp.Domain.Entities;
+
+namespace FinancasApp.Domain.Interfaces.Messages
+{
+    public interface IMessageProducer
+    {
+        /// <summary>
+        /// Metodo para enviar a movimentação para a fila de mensagens.
+        /// </summary>
+        
+        void Send(Movimentacao movimentacao);
+
+    }
+}
